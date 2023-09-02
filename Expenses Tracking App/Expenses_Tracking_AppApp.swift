64d8a6d1bddac14eedfa6de5f991e6a0e8df43f1
PathiranaAshen <pathiashen@gmail.com>
2023-09-02 17:56:58 +0530

    Initial Commit

---
+++ Expenses Tracking App/Expenses_Tracking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses_Tracking_AppApp.swift
+//  Expenses Tracking App
+//
+//  Created by Mihir Ben on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct Expenses_Tracking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
